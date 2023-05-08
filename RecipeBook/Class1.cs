058dbac8b79edb380a23769b1f9901854224965e
Spicy-Oreo <81910721+Spicy-Oreo@users.noreply.github.com>
2023-05-08 15:25:30 +0300

    Added new choice method along with the calling of new future methods that will be added

--- RecipeBook/Class1.cs
+++ RecipeBook/Class1.cs
@@ -55,6 +55,8 @@ namespace RecipeBook
                     Array.Resize(ref IngredAmount, size);
                     Array.Resize(ref IngredMeasurement, size);
 
+
+                    //for loop to loop through the populating of ingredient details
                     for (int i = 0; i < IngredName.Length; i++)
                     {
                         Console.WriteLine("Please enter the ingredient name");
@@ -75,6 +77,7 @@ namespace RecipeBook
                 }
 
 
+                //Else statement to restart the method if the user input was not in the correct format
                 else
                 {
                     Console.WriteLine("Invalid input for number of ingredients please try again");
@@ -82,7 +85,7 @@ namespace RecipeBook
 
                 }
             }
-            catch(Exception ex) 
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
@@ -102,164 +105,87 @@ namespace RecipeBook
             try
             {
 
-            
-               Console.WriteLine("Please enter the number of steps");
-               usrNumSteps = Console.ReadLine();
 
-                 if (Regex.IsMatch(usrNumSteps, @"^\d$"))
-                 {
+                Console.WriteLine("Please enter the number of steps");
+                usrNumSteps = Console.ReadLine();
+
+                //If statment to verify user entered a number
+                if (Regex.IsMatch(usrNumSteps, @"^\d$"))
+                {
+                    //Converting of user input to int
                     numSteps = Convert.ToInt32(usrNumSteps);
-    
+
+                    ///Resizing the array based on the user input
                     Array.Resize(ref IngredSteps, numSteps);
 
-                     for (int i = 0; i < IngredSteps.Length; i++)
-                     {
-                     Console.WriteLine("Please enter step " + (1 + i));
-                     string stepDetails = Console.ReadLine();
-                     IngredSteps[i] = stepDetails;
-                     }
-                 }
-
-                  else
-                  {
+                    //For loop to populate the steps with instructions
+                    for (int i = 0; i < IngredSteps.Length; i++)
+                    {
+                        Console.WriteLine("Please enter step " + (1 + i));
+                        string stepDetails = Console.ReadLine();
+                        IngredSteps[i] = stepDetails;
+                    }
+                }
+                //else statment to restart the method if a number value was not entered 
+                else
+                {
                     Console.WriteLine("Invalid input please enter a number");
                     Instructions();
-                  }
+                }
             }
 
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.Message);  
-            }
-        }
-        /// <summary>
-        /// End of the method
-        /// </summary>
-
-
-
-        /// <summary>
-        /// Method used to allow the user to increase the scale of the recipe or print with the original details
-        /// </summary>
-        public void Choice()
-        {
-            try
-            {
-
-            
-            Console.WriteLine("Would you like to increase the scale ? Enter 1 for no or any other digit as the scale you want to increase the recipe by");
-            string input = Console.ReadLine();
-
-            ///If user choses 1 it will print the original choice 
-            if(input == "1")
-            {
-                PrintOriginal();
-            }
-
-            //If the input is a digit it will be the factor at which the ingredients are multiplied by 
-            else if (Regex.IsMatch(input,@"^\d$"))
-            {
-                inputHere = Convert.ToInt32(input);
-                PrintScaledUp();
-
-            }
-
-            //If the input is not a digit it will prompt the user of the error and re-run the method so the user can re-enter in the correct format (digit)
-            else
-            {
-                Console.WriteLine("Invalid input please enter a digit");
-                Choice();
-            }
-            }
-            catch(Exception ex) 
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
         }
         /// <summary>
-        /// End of method 
-        /// </summary>
-
-
-
-
-        /// <summary>
-        /// Method used to print the recipe based on what it is scaled up by 
-        /// </summary>
-        public void PrintScaledUp()
-        {
-            Console.WriteLine("------------------------------------------------------------------------------------------------");
-            Console.WriteLine("Ingredients");
-            Console.WriteLine("-----------");
-            for (int i = 0; i < IngredName.Length; i++)
-            {
-                string IngredientNameHere = IngredName[i];
-                string IngredientMeasurementHere = IngredMeasurement[i];
-                double IngredientAmountHere = (IngredAmount[i]*inputHere);
-                Console.WriteLine(IngredientAmountHere + " " + IngredientMeasurementHere + " " + IngredientNameHere);
-            }
-            Console.WriteLine("------------------------------------------------------------------------------------------------");
-            Console.WriteLine("Steps");
-            Console.WriteLine("-----");
-            for (int i = 0; i < IngredSteps.Length; i++)
-            {
-                Console.WriteLine("Step " + (i + 1) + ": " + IngredSteps[i]);
-            }
-            Console.WriteLine("------------------------------------------------------------------------------------------------");
-            Console.WriteLine("");
-            Console.WriteLine("Would you like to return to the orignal values ? Enter 1 for yes or any other key to exit");
-            string originalInput = Console.ReadLine();
-            if(originalInput == "1")
-            {
-                PrintOriginal();
-            }
-            else
-            {
-               
-            }
-        }
-        /// <summary>
-        /// End of method 
-        /// </summary>
-
-
-
-
-
-
-        /// <summary>
-        /// Method used to print the oringal recipe scale 
-        /// </summary>
-        public void PrintOriginal()
-        {
-            Console.WriteLine("------------------------------------------------------------------------------------------------");
-            Console.WriteLine("Ingredients");
-            Console.WriteLine("-----------");
-            for (int i = 0; i < IngredName.Length; i++)
-            {
-               string IngredientNameHere = IngredName[i];
-               string IngredientMeasurementHere = IngredMeasurement[i];
-               double IngredientAmountHere = IngredAmount[i];
-               Console.WriteLine(IngredientAmountHere + " "+IngredientMeasurementHere + " " + IngredientNameHere);
-            }
-            Console.WriteLine("------------------------------------------------------------------------------------------------");
-            Console.WriteLine("Steps");
-            Console.WriteLine("-----");
-            for (int i = 0; i < IngredSteps.Length;i++)
-            {
-                Console.WriteLine("Step "+(i+1)+": "+IngredSteps[i]);
-            }
-            Console.WriteLine("------------------------------------------------------------------------------------------------");
-        }
-        /// <summary>
         /// End of the method
         /// </summary>
 
+        public void Choice()
+        {
+            try
+            {
+
+
+                Console.WriteLine("Would you like to increase the scale ? Enter 1 for no or any other digit as the scale you want to increase the recipe by");
+                string input = Console.ReadLine();
+
+                ///If user choses 1 it will print the original choice 
+                if (input == "1")
+                {
+                    PrintOriginal();
+                }
+
+                //If the input is a digit it will be the factor at which the ingredients are multiplied by 
+                else if (Regex.IsMatch(input, @"^\d$"))
+                {
+                    inputHere = Convert.ToInt32(input);
+                    PrintScaledUp();
+
+                }
+
+                //If the input is not a digit it will prompt the user of the error and re-run the method so the user can re-enter in the correct format (digit)
+                else
+                {
+                    Console.WriteLine("Invalid input please enter a digit");
+                    Choice();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
+        /// <summary>
+        /// End of method 
+        /// </summary>
+
+
+
 /// <summary>
-/// End of file 
+/// Method used to allow the user to increase the scale of the recipe or print with the original details
 /// </summary>
-
-
-
