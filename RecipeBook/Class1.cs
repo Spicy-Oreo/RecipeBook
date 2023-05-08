f1008b1020a1ac40689a00306c77fc681e666037
Spicy-Oreo <81910721+Spicy-Oreo@users.noreply.github.com>
2023-05-08 13:32:27 +0300

    Add project files.

---
+++ RecipeBook/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeBook
+{
+    internal class Class1
+    {
+        string[] IngredName = new string[0];
+        int[] IngredAmount = new int[0];
+        string[] IngredMeasurement = new string[0];
+        string[] Steps = new string[0];
+
+        string ingredientName = "";
+        string uMeasurement = "";
+        string recipeName = "";
+
+        int ingredientAmount = 0;
+
+        string usrNumSteps = "";
+        int numSteps = 0;
+
+        public void RequiredIngredients()
+        {
+            Console.WriteLine("Please enter the recipe name");
+            recipeName = Console.ReadLine();
+
+            Console.WriteLine("Please enter the number of ingredients");
+            string usrNumIngredient = Console.ReadLine();
+
+            int numIngredients = Convert.ToInt32(usrNumIngredient);
+
+            int size = numIngredients;
+
+            Array.Resize(ref IngredName, size);
+            Array.Resize(ref IngredAmount, size);
+            Array.Resize(ref IngredMeasurement, size);
+
+            for (int i = 0; i < IngredName.Length; i++)
+            {
+                Console.WriteLine("Please enter the ingredient name");
+                ingredientName = Console.ReadLine();
+                IngredName[i] = ingredientName;
+
+                Console.WriteLine("Please enter the ingredient amount");
+                string usringredientAmount = Console.ReadLine();
+
+                ingredientAmount = Convert.ToInt32(usringredientAmount);
+                IngredAmount[i] = ingredientAmount;
+
+                Console.WriteLine("Please enter the ingredient unit of measurement (e.g tablespoon/teaspoon)");
+                uMeasurement = Console.ReadLine();
+                IngredMeasurement[i] = uMeasurement;
+            }
+        }
+    }
+}
---
+++ RecipeBook/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace RecipeBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, welcome to My Recipe Book!");
+            Class1 c1 = new Class1();
+            c1.RequiredIngredients();
+
+
+            Console.ReadLine();
+        }
+    }
+}
